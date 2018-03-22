f60eb17649ecfd7c24d98a551df091ed91c4f7ca
NiclasB <Niclas1.h>
2018-03-22 17:24:33 +0100

    Change of navbar and index page changes

---
+++ ITMatcherWeb/Models/ConsultantExperience.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMatcherWeb.Models
+{
+    public class ConsultantExperience
+    {
+        String Employer { get; set; }
+        DateTime DateOfEmployment { get; set; }
+        DateTime DateOfExit { get; set; }
+        List<Subject> Subjects { get; set; }
+
+
+
+    }
+
+    internal class Subject
+    {
+        String Name { get; set; }
+        DateTime StartTime { get; set; }
+        DateTime DateTime { get; set; }
+        int PercievedLevelOfSkill { get; set; }
+    }
+
+
+}

73f14dd44ef9fbec761ff6da187da13e29042acc
NiclasB <Niclas1.h>
2018-04-27 21:56:21 +0200

    Cookie warning added

--- ITMatcherWeb/Controllers/ProfileController.cs
+++ ITMatcherWeb/Controllers/ProfileController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ITMatcherWeb.Controllers
 {
     public class ProfileController : Controller
     {
+        //Get ID of current user -> Get role of current user -> Pass to view.
         // GET: Profile
+        [Authorize]
         public ActionResult Profile()
         {
             return View("profilePage");
